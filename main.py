b408c472ab92cf59000e97a9caa215d982aa142b
DiegoYoshida <diegokenjiyoshida2508@gmail.com>
2025-06-04 21:40:15 -0300

    Revert "feat: Declaração da rota /analyse"
    This reverts commit cd4a4adab486ad48b56032995df0678fc0c9540b.

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-from flask import Flask, render_template, request  
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
-
-if __name__ == "__main__": 
-    app.run(host = "0.0.0.0", port = 8080)
-
-@app.route('/analyse', methods = ["POST"]) 
-def analyse():
-    id_msg, msg = request.get_json().values()
-    return id_msg + ' ' + msg
-
-
-    
-  
