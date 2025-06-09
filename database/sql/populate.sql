87a5083688090cf965a4cc5fedfbe3857bb3bc33
Vinicius Lima <vinicius.limavl001@gmail.com>
2025-06-09 20:24:35 -0300

    feat: Adicionando mensagem na tela de login, e corrigindo arquivo para popular dados no banco.

--- database/sql/populate.sql
+++ database/sql/populate.sql
@@ -1,8 +1,8 @@
 BEGIN TRANSACTION;
 
 INSERT INTO Usuario VALUES ('ce23e1cb-097d-4196-adb7-d32fe78aa642','cleber','1234','30/05/2025');
-INSERT INTO Post VALUES ('f73fb552-5791-42fe-9234-f09efb1bda1e','Como ter internet de graça','Preciso para amanhã','30/05/2025','ce23e1cb-097d-4196-adb7-d32fe78aa642',0);
-INSERT INTO Resposta VALUES ('f73fb552-5791-42fe-9234-f09efb1bda1e','f73fb552-5791-42fe-9234-f09efb1bda1e','Não tem como, todos os métodos hoje, são pagos.', 'ce23e1cb-097d-4196-adb7-d32fe78aa642', '07/06/2025 13:40');
+INSERT INTO Post VALUES ('f73fb552-5791-42fe-9234-f09efb1bda1e','Como abastecer sem parar para pagar','Preciso para uma viagem','30/05/2025, 10:30:20','ce23e1cb-097d-4196-adb7-d32fe78aa642',0);
+INSERT INTO Resposta VALUES ('f73fb552-5791-42fe-9234-f09efb1bda1e','f73fb552-5791-42fe-9234-f09efb1bda1e','Compre uma TAG sem parar em algum estabelecimento...', 'ce23e1cb-097d-4196-adb7-d32fe78aa642', '07/06/2025 13:40:20');
 INSERT INTO Tag VALUES ('cultura', '30/05/2025 14:30');
 
 COMMIT;
